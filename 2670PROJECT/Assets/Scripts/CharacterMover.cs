db8a7ccce52b4223b5015e915c12a2ad4f6f7b2f
Derek <10873409@my.uvu.edu>
2020-08-31 21:33:10 -0600

    Added Jump and Move

--- 2670PROJECT/Assets/Scripts/CharacterMover.cs
+++ 2670PROJECT/Assets/Scripts/CharacterMover.cs
@@ -1,19 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Timeline;
+[RequireComponent(typeof(CharacterController))]
 public class CharacterMover : MonoBehaviour
 {
-    public float testFloat = 50f;
+    private CharacterController controller;
+    private Vector3 movement;
+    public float gravity = 10f;
+    public float moveSpeed = 3f;
+    public float sprintMoveSpeed;
+    public float jumpForce = 10f;
+    public int jumpCountMax;
+    
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World");
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+        
+        if (Input.GetButtonDown("Jump"))
+        {
+            movement.y = jumpForce;
+        }
+
+        if (controller.isGrounded)
+        {
+            movement.y = 0;
+        }
+        else
+        {
+            movement.y -= gravity;  
+        }
+        
+        movement.x = Input.GetAxis("Horizontal")*moveSpeed;
+        controller.Move(movement*Time.deltaTime);
     }
 }
