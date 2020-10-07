c97c7203d6986dd9435f56ddb8fe747feceab7c7
Derek <10873409@my.uvu.edu>
2020-10-06 22:53:54 -0600

    Moving Platform sorta

---
+++ 2670PROJECT/Assets/Scripts/Behaviors/PlatformMoveBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlatformMoveBehavior : MonoBehaviour
+{
+    private CharacterController controller;
+    public Vector3Data pos1, pos2;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    //private void Update()
+    //{
+        //controller.Move(pos1.value);
+    //}
+}
