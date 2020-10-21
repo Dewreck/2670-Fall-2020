7b73df44f7e5f13b51d56866d254dba3967817db
Derek <10873409@my.uvu.edu>
2020-10-21 14:09:19 -0600

    Worked on State Machine

---
+++ 2670PROJECT/Assets/Scripts/Behaviors/CharacterWithStates.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class CharacterWithStates : MonoBehaviour
+{
+    private CharacterController controller;
+
+    public CharacterStateMachineData characterStates;
+    
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        switch (characterStates.value)
+        {
+            case CharacterStateMachineData.characterStates.StandardWalk:
+                break;
+            case CharacterStateMachineData.characterStates.WallCrawl:
+                break;
+            case CharacterStateMachineData.characterStates.Idle:
+                break;
+            
+        }
+    }
+
+    
+}
---
+++ 2670PROJECT/Assets/Scripts/ScriptableObjects/CharacterStateMachineData.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class CharacterStateMachineData : ScriptableObject
+{
+    public enum characterStates
+    {
+        StandardWalk,
+        WallCrawl,
+        Idle
+    }
+    
+    public characterStates value;
+
+    
+}
