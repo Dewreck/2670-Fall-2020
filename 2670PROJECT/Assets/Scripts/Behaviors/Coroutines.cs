8e9765692b8271a2aca6efd036b33779fc319c36
Derek <10873409@my.uvu.edu>
2020-10-26 13:28:27 -0600

    Moving Platform better

--- 2670PROJECT/Assets/Scripts/Behaviors/Coroutines.cs
+++ 2670PROJECT/Assets/Scripts/Behaviors/Coroutines.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class Coroutines : MonoBehaviour
 {
-    public int counter = 100;
+    public int counter = 100, counterMax = 100;
     private WaitForFixedUpdate wffu = new WaitForFixedUpdate();
     private WaitForSeconds wfs = new WaitForSeconds(1f);
     private IEnumerator Start()
@@ -19,7 +19,7 @@ public class Coroutines : MonoBehaviour
         
         yield return wfs;
         
-        while (counter < 100)
+        while (counter < counterMax)
         {
             yield return wffu;
             transform.Translate(-0.1f,0,0);
